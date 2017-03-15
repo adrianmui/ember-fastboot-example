981157658e644f0221e135bab37d2b853248dfab
Adrian Mui <amui@omeda.com>
2017-03-15 11:12:30 -0500

    haven't pushed in awhile..silly project pause

--- app/controllers/directory.js
+++ app/controllers/directory.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    actions: {
-        submit() {
-        }
+    queryParams: ['search'],
+    search: {
+        refreshModel: true
+    },
+    model: function(params) {
+        
     }
 });
